f828052abe144662c094282c1d83c6064d925839
binghuan <binghuanlin@gmail.com>
2018-09-27 15:26:56 +0800

    add concept.

--- CodingTest_2/meetingRoomArrangment.js
+++ CodingTest_2/meetingRoomArrangment.js
@@ -1,4 +1,18 @@
 
+/* Concept. 
+
+1. Allocate Seat Map for Meeting Room by Width and Height
+2. Take number of seats by checking the Neighbor, less is better. 
+3. Try 2 solution for taking seat. 
+	Solution [1]: Taking seat from first. 
+	Solution [2]: Taking seat from last. 
+
+Do the comparison between solution [1] and solution [2]. 
+
+Get the min. result for answer. 
+
+*/
+
 const DBG = false;
 
 let seatMap = [];
